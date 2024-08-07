fa23eff26dc151b7b651e0aadadb43e80bc587fc
perturbing <thomas.vellekoop@iohk.io>
2024-08-07 11:35:35 +0200

    make working example of rust -> C -> haskell binding

--- rust-c-bindings/lib/MyBindings.hs
+++ rust-c-bindings/lib/MyBindings.hs
@@ -1,4 +1,12 @@
-module MyBindings where
+{-# LANGUAGE ForeignFunctionInterface #-}
 
-test :: Integer
-test = 42
+module MyBindings (addViaC) where
+
+import Foreign.C.Types
+
+-- Import the Rust function
+foreign import ccall "add" c_add :: CInt -> CInt -> CInt
+
+-- Haskell wrapper function
+addViaC :: Int -> Int -> Int
+addViaC x y = fromIntegral (c_add (fromIntegral x) (fromIntegral y))
--- rust-c-bindings/src/Main.hs
+++ rust-c-bindings/src/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import MyBindings (addViaC)
+import System.IO (hFlush, stdout)
+
+-- Function to prompt the user for an integer
+promptForInt :: String -> IO Int
+promptForInt prompt =
+    do putStr prompt
+      hFlush
+      stdout
+      read
+        <$> getLine
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- Prompt the user for two integers
+    putStrLn "Hello, Haskell!"
+    x <- promptForInt "Enter the first integer: "
+    y <- promptForInt "Enter the second integer: "
+
+    -- Call the addViaC function
+    let result = addViaC x y
+
+    -- Print the result
+    putStrLn $ "The result of adding " ++ show x ++ " and " ++ show y ++ " via the C binding is: " ++ show result
