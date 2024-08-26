235b42ac89648059a61eac33d62094305f701f58
perturbing <thomas.vellekoop@iohk.io>
2024-08-26 14:46:30 +0200

    add example on list of Fr elements

--- rust-c-bindings/lib/MyBindings.hs
+++ rust-c-bindings/lib/MyBindings.hs
@@ -1,37 +1,78 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module MyBindings (doubleFr) where
+module MyBindings (randomScalarList) where
 
 import Cardano.Crypto.EllipticCurve.BLS12_381.Internal (Fr (..), sizeFr)
+import Control.Monad (forM_)
+import Data.Traversable (forM)
 import Data.Void (Void)
 import Data.Word (Word8)
-import Foreign.C.Types
-import Foreign.ForeignPtr (castForeignPtr, newForeignPtr, withForeignPtr)
-import Foreign.Marshal.Alloc (finalizerFree, mallocBytes)
+import Foreign (ForeignPtr)
+import Foreign.C.Types (CSize (..))
+import Foreign.ForeignPtr (mallocForeignPtrBytes, newForeignPtr, newForeignPtr_, withForeignPtr)
+import Foreign.Marshal (copyBytes)
+import Foreign.Marshal.Alloc (finalizerFree)
 import Foreign.Marshal.Array (peekArray)
-import Foreign.Ptr (Ptr, castPtr, nullPtr)
+import Foreign.Ptr (Ptr, castPtr, nullPtr, plusPtr)
+import GHC.Arr (listArray)
 
-foreign import ccall "random_scalar" c_random_scalar :: Ptr Void -> IO (Ptr Void)
+-- Foreign function interface for the Rust function
+foreign import ccall "random_scalar_list" c_random_scalar_list :: Ptr Void -> CSize -> IO ()
 
+-- Helper function to view bytes of a foreign pointer
 viewBytes :: Ptr a -> Int -> IO [Word8]
 viewBytes ptr size = peekArray size (castPtr ptr :: Ptr Word8)
 
-doubleFr :: Fr -> IO Fr
-doubleFr (Fr fp) = withForeignPtr fp $ \ptr -> do
-    if ptr == nullPtr
-        then putStrLn "Pointer is null before calling Rust function!"
-        else putStrLn $ "Pointer is valid: " ++ show ptr
+-- Function to handle a list of `Fr` values
+randomScalarList :: [Fr] -> IO [Fr]
+randomScalarList frList = do
+    if null frList
+        then return []
+        else do
+            let lengthFr = length frList
+            -- Combine the raw pointers into a single list
+            ptrList <- concatFr frList
+            -- Ensure all pointers are valid
+            withForeignPtr ptrList $ \ptr -> do
+                -- Call the Rust function with the pointer and length
+                let len = fromIntegral lengthFr :: CSize
+                c_random_scalar_list ptr len
 
-    originalValues <- viewBytes ptr sizeFr
-    putStrLn $ "Original Values: " ++ show originalValues
+                -- Convert `Ptr Void` back to `ForeignPtr Void` without using `finalizerFree`
+                resultFps <- newForeignPtr_ ptr
 
-    resultPtr <- c_random_scalar ptr
-    if resultPtr == nullPtr
-        then putStrLn "Received null pointer back from Rust function!"
-        else putStrLn $ "Result Pointer is valid: " ++ show resultPtr
+                -- Split the `ForeignPtr Void` into a list of `Fr`
+                splitFr resultFps lengthFr
 
-    resultValues <- viewBytes resultPtr sizeFr
-    putStrLn $ "Result Values: " ++ show resultValues
+concatFr :: [Fr] -> IO (ForeignPtr Void)
+concatFr frList = do
+    -- Calculate total size needed
+    let totalSize = sizeFr * length frList
 
-    resultFp <- newForeignPtr finalizerFree resultPtr
-    return (Fr resultFp)
+    -- Allocate the final large ForeignPtr
+    finalPtr <- mallocForeignPtrBytes totalSize
+
+    -- Copy each Fr element into the finalPtr
+    withForeignPtr finalPtr $ \destPtr -> do
+        let copyElement offset (Fr srcPtr) = withForeignPtr srcPtr $ \src ->
+                copyBytes (destPtr `plusPtr` offset) src sizeFr
+
+        -- Use a forM_ loop to copy each element
+        forM_ (zip [0, sizeFr ..] frList) $ uncurry copyElement
+
+    -- Return the final ForeignPtr
+    return finalPtr
+
+-- Function to split a ForeignPtr containing multiple Fr elements into a list of individual Fr elements
+splitFr :: ForeignPtr Void -> Int -> IO [Fr]
+splitFr srcPtr n = do
+    -- Iterate over the number of elements and extract each `Fr`
+    withForeignPtr srcPtr $ \src ->
+        forM [0 .. n - 1] $ \i -> do
+            -- Allocate memory for each individual `Fr`
+            frPtr <- mallocForeignPtrBytes sizeFr
+            -- Copy the corresponding bytes from the original buffer
+            withForeignPtr frPtr $ \dst ->
+                copyBytes dst (src `plusPtr` (i * sizeFr)) sizeFr
+            -- Return the new `Fr`
+            return $ Fr frPtr
--- rust-c-bindings/src/Main.hs
+++ rust-c-bindings/src/Main.hs
@@ -1,13 +1,42 @@
 module Main where
 
 import Cardano.Crypto.EllipticCurve.BLS12_381.Internal (Scalar (..), frFromScalar, scalarFromFr, scalarFromInteger, scalarToInteger)
-import MyBindings (doubleFr)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import GHC.IO (unsafePerformIO)
+import qualified Hedgehog.Internal.Gen as G
+import qualified Hedgehog.Internal.Range as R
+import MyBindings (randomScalarList)
+
+byteStringToInteger :: ByteString -> Integer
+byteStringToInteger = BS.foldl' (\acc b -> acc * 256 + fromIntegral b) 0
+
+listOfSizedIntegers :: Integer -> Integer -> [Integer]
+listOfSizedIntegers n l =
+    unsafePerformIO
+        . G.sample
+        $ ( map byteStringToInteger
+                <$> G.list
+                    (R.singleton $ fromIntegral n)
+                    (G.bytes (R.singleton $ fromIntegral l))
+          )
 
 main :: IO ()
 main = do
-    scalar <- scalarFromInteger 44088922575230112513242719452924342913839627358734666446284982958943915819480
-    fr <- frFromScalar scalar
-    resultFr <- doubleFr fr
-    resultScalar <- scalarFromFr resultFr
-    y <- scalarToInteger resultScalar
-    putStrLn $ "The random scalar via the C binding is: " ++ show y
+    -- A list of the integer 0 to 100
+    -- let integers = [0..10000000]
+    -- Generate a list random integers with 31 bytes each
+    let integers = listOfSizedIntegers 1000 31
+
+    -- Convert the integers to Scalars
+    scalars <- mapM scalarFromInteger integers
+
+    -- Convert the Scalars to Fr values
+    frs <- mapM frFromScalar scalars
+
+    -- Use randomScalarList on list of values
+    resultFrs <- randomScalarList frs
+    let lastElement = last resultFrs
+    lastScalar <- scalarFromFr lastElement
+    lastInteger <- scalarToInteger lastScalar
+    print lastInteger
